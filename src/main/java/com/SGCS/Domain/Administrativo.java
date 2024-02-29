ef97e7de15b3a258897655abb984f0abbdbce6eb
AngelArribasplata <aarribasplatae17_1@unc.edu.pe>
2024-02-29 18:30:00 -0500

    CONFIGURANDO

--- src/main/java/com/SGCS/Domain/Administrativo.java
+++ src/main/java/com/SGCS/Domain/Administrativo.java
@@ -1,4 +1,10 @@
 package com.SGCS.Domain;
 
 public class Administrativo {
+
+    private String Nombre;
+    private String Apellido;
+    private String Cargo;
+    private String Telefono;
+    private String Email;
 }
