0a17999fa24e78d59fb8fe0f4ddd90a04dd867f3
Jhon Bravo <jbravol17_1@unc.edu.pe>
2024-02-29 22:48:57 -0500

    creando HisMedDTO

---
+++ src/main/java/com/SGCS/DTO/HistorialMedicoDTO.java
@@ -0,0 +1,14 @@
+package com.SGCS.DTO;
+
+import com.SGCS.Domain.Paciente;
+import lombok.Data;
+
+@Data
+public class HistorialMedicoDTO {
+private  Long idHistorial;
+private Paciente paciente;
+private String alergias;
+private String enfermedadesPrevias;
+private String cirugias;
+private String medicacionHabitual;
+}
