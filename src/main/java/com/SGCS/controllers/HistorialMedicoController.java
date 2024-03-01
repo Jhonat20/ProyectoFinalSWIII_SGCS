3ff8f5a68fa77bdf989bdf28853bae6d63c642f9
Jhon Bravo <jbravol17_1@unc.edu.pe>
2024-03-01 16:04:42 -0500

    creando controllador y el ErrorResponse actualizando

--- src/main/java/com/SGCS/controllers/HistorialMedicoController.java
+++ src/main/java/com/SGCS/controllers/HistorialMedicoController.java
@@ -1,6 +1,72 @@
 package com.SGCS.controllers;
 
-public class HistorialMedicoController {
 
+import com.SGCS.Domain.HistorialMedico;
+import com.SGCS.exception.ErrorResponse;
+import com.SGCS.exception.IllegalOperationException;
+import com.SGCS.services.HistorialMedicoService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/HistrialMedico")
+public class HistorialMedicoController {
+ @Autowired
+private HistorialMedicoService historialMedicoService;
+
+ @GetMapping
+public ResponseEntity<?> obtenerTodos() {
+     try {
+         List<HistorialMedico> historialMedico = historialMedicoService.listarTodos();
+         if(historialMedico==null || historialMedico.isEmpty()) {
+         	return ResponseEntity.noContent().build();
+         }
+         else return ResponseEntity.ok(historialMedico);
+     } catch (Exception e) {
+     	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Error interno del servidor"));
+     }
+ }
+@GetMapping("/{id}")
+    public ResponseEntity<?> obtenerPorId(@PathVariable Long id) {
+     try {
+         HistorialMedico historialMedico = historialMedicoService.buscarPorId(id);
+         return ResponseEntity.ok(historialMedico);
+
+     }catch (EntityNotFoundException e) {
+     	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
+     }catch (Exception e) {
+         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Error interno del servidor"));
+     }
+ }
+
+@PostMapping
+public ResponseEntity<?> guardar(@RequestBody HistorialMedico historialMedico) {
+     try {
+         historialMedico = historialMedicoService.grabar(historialMedico);
+         return ResponseEntity.status(HttpStatus.CREATED).body(historialMedico);
+     } catch(IllegalOperationException e) {
+     	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+     } catch (Exception e) {
+     	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Error interno del servidor"));
+     }
+ }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> actualizar(@PathVariable Long id, @RequestBody HistorialMedico historialMedico) {
+        try {
+            historialMedico = historialMedicoService.actualizar(id, historialMedico);
+            return ResponseEntity.ok(historialMedico);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
+        } catch (IllegalOperationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Error interno del servidor"));
+        }
+    }
 
 }
--- src/main/java/com/SGCS/exception/ErrorResponse.java
+++ src/main/java/com/SGCS/exception/ErrorResponse.java
@@ -1,5 +1,20 @@
+/**
+ * @file: ErrorResponse.java
+ * @author: (c)2024 evalencia 
+ * @created: Feb 20, 2024 11:55:00 PM
+ */
 package com.SGCS.exception;
 
-public class ErrorResponse {
+import lombok.Data;
 
+/**
+ * 
+ */
+@Data
+public class ErrorResponse {
+	private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
 }
